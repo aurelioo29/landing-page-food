234f397b0e65a86d56c49f0fb69f9a1fccd043ff
aurelioo29 <aureliolucio5@gmail.com>
2024-10-03 15:26:08 +0700

    test(slider): Adding slider card [unused]

---
+++ src/lib/slider/slider.tsx
@@ -0,0 +1,22 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import React from "react"; // Pastikan ini ada
+
+const HeroSlider = ({ children }: any) => {
+  return (
+    <Splide
+      options={{
+        type: "loop",
+        perPage: 1,
+        autoplay: true,
+        pauseOnHover: false,
+      }}
+    >
+      {React.Children.toArray(children).map((child, index) => (
+        <SplideSlide key={index}>{child}</SplideSlide>
+      ))}
+    </Splide>
+  );
+};
+
+export default HeroSlider;
